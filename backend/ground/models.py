a38fbb78e4c5d88834e041c01887ef8f522ef8d7
Bruno Teles <brunomadteles@gmail.com>
2023-10-26 16:51:25 +0100

    Started thinking about building game model (no migration yet)

--- backend/ground/models.py
+++ backend/ground/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class Player(models.Model):
     nickname = models.CharField(max_length=20, unique=True)
+
+class Game(models.Model):
+    # Player1 = Player.ForeignKey
+    # Player2 = Player.ForeighKey
+    # Gamestate somehow. Nine variables? Sq00, Sq01, s02, Sq10, Sq11 ...
+    pass
+
