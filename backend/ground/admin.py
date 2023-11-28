6700a7eec1c1650b77e2488aa0ecf6e1545a43fa
Bruno Teles <brunomadteles@gmail.com>
2023-11-28 20:23:46 +0000

    Registered new models in admin page

--- backend/ground/admin.py
+++ backend/ground/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Player
+from .models import Player, TicTacToeMatrix, Game
 admin.site.register(Player)
+admin.site.register(TicTacToeMatrix)
+admin.site.register(Game)
