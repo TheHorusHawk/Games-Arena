27b7ac58196dd52a00df9938babd8c600c2c8527
Bruno Teles <brunomadteles@gmail.com>
2023-12-01 15:36:36 +0000

    Added support for activePlayer to play

---
+++ backend/ground/static/ground/tictactoeactive.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+    squares = document.querySelectorAll(".square")
+    squares.forEach(
+        (square) => {
+            if (square.innerHTML == "0") {
+                square.innerHTML = "Button"
+            }
+        }
+
+    )
+//     use edit buttons to activate edit mode 
+//     let in_button = document.getElementById("in_button");
+//     in_button.innerHTML = "Javascript loaded"
+//     /*in_out_button.forEach((currentValue, currentIndex, listObj) => {
+//         currentValue.addEventListener('click', (value) => get_in(value))
+//     })*/
+//     in_button.addEventListener('click', (x) => get_in(x))
+});
+
+// function get_in(event) {
+//     let button = event.target;
+//     button.innerText = 'Get out!';
+//     console.log(button.innerHTML)
+//     console.log("Clicked3")
+//     fetch('toggle_in', {
+//         method: 'POST',
+//         body: JSON.stringify({
+//         })
+//     })
+// }
